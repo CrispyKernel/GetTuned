dd167977e4b0502e322c2ac1aecb5c3b06ee3ee7
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2020-04-26 16:55:37 -0400

    Changes to __init__.py

--- GetTuned/__init__.py
+++ GetTuned/__init__.py
@@ -1,4 +1,4 @@
 
 from HPtuner import HPtuner, DiscreteDomain, ContinuousDomain
 from Model import MLP, SVM, CnnVanilla, ResNet
-from DataManager import DataGenerator, load_breast_cancer_dataset
+import DataManager
