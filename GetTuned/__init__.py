dc4f0e14bc8116b1b5aaefaa957ec54a55cf9505
Rayn2402 <nicolas.raymon2@usherbrooke.ca>
2020-04-30 11:59:36 -0400

    load data function added to init

--- GetTuned/__init__.py
+++ GetTuned/__init__.py
@@ -1,6 +1,6 @@
 
 from .DataManager import DataGenerator, load_cifar10, load_cifar100, load_csv, load_svhn, load_stl10, \
-     load_mnist, load_forest_covertypes_dataset, load_breast_cancer_dataset, load_digits_dataset
+     load_mnist, load_forest_covertypes_dataset, load_breast_cancer_dataset, load_digits_dataset, plot_data
 
 from .HPtuner import HPtuner, DiscreteDomain, ContinuousDomain
 from .Model import SVM, MLP, CnnVanilla, ResNet
